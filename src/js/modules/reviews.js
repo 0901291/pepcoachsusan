78a954b6d0337ee1356fb64a73983968a5d8479e
Ian Wensink <ianwensink@gmail.com>
2016-08-21 00:11:03 +0200

    Misc: - Reviews carousel more responsive - Slogan in header responsive fixes

--- src/js/modules/reviews.js
+++ src/js/modules/reviews.js
@@ -23,12 +23,18 @@ function initSlick_() {
             pauseOnHover: true,
             swipeToSlide: true,
             infinite: true,
+            centerMode: true,
             arrows: false,
+            prevArrow: '<button type="button" class="reviews__nav--prev"><svg><use xlink:href="#chevron"></use></svg></button>',
+            nextArrow: '<button type="button" class="reviews__nav--next"><svg><use xlink:href="#chevron"></use></svg></button>',
+            appendArrows: '.reviews__nav',
             responsive: [
                 {
                     breakpoint: utils.SCREEN_SIZES.smallDesktop,
                     settings: {
                         centerPadding: "20%",
+                        arrows: true,
+                        centerMode: true,
                     }
                 },
                 {
@@ -37,9 +43,6 @@ function initSlick_() {
                         centerMode: true,
                         centerPadding: "10%",
                         arrows: true,
-                        prevArrow: '<button type="button" class="reviews__nav--prev"><svg><use xlink:href="#chevron"></use></svg></button>',
-                        nextArrow: '<button type="button" class="reviews__nav--next"><svg><use xlink:href="#chevron"></use></svg></button>',
-                        appendArrows: '.reviews__nav',
                     }
                 }
             ]
