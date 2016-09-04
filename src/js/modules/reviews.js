38ef3eecbab6c326f4381dd95a62d2b936d923a8
Ian Wensink <ianwensink@gmail.com>
2016-09-04 16:14:41 +0200

    Fix reviews slider bug where container height got calculated too soon, so before slider was initialized

--- src/js/modules/reviews.js
+++ src/js/modules/reviews.js
@@ -11,50 +11,46 @@ export default function initReviews() {
 }
 
 function initSlick_() {
-    // if(!$reviewsContainer.hasClass('slick-initialized') && $(window).width() > utils.SCREEN_SIZES.mobile) {
-        $reviewsContainer.slick({
-            slidesToShow: 1,
-            autoplay: true,
-            autoplaySpeed: 10000,
-            dots: true,
-            centerPadding: "0%",
-            slidesToShow: 1,
-            mobileFirst: true,
-            pauseOnHover: true,
-            swipeToSlide: true,
-            infinite: true,
-            centerMode: true,
-            arrows: false,
-            prevArrow: '<button type="button" class="reviews__nav--prev"><svg><use xlink:href="#chevron"></use></svg></button>',
-            nextArrow: '<button type="button" class="reviews__nav--next"><svg><use xlink:href="#chevron"></use></svg></button>',
-            appendArrows: '.reviews__nav',
-            responsive: [
-                {
-                    breakpoint: utils.SCREEN_SIZES.smallDesktop,
-                    settings: {
-                        centerPadding: "20%",
-                        arrows: true,
-                        centerMode: true,
-                    }
-                },
-                {
-                    breakpoint: utils.SCREEN_SIZES.tablet,
-                    settings: {
-                        centerMode: true,
-                        centerPadding: "10%",
-                        arrows: true,
-                    }
+    $reviewsContainer.slick({
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 10000,
+        dots: true,
+        centerPadding: "0%",
+        slidesToShow: 1,
+        mobileFirst: true,
+        pauseOnHover: true,
+        swipeToSlide: true,
+        infinite: true,
+        centerMode: true,
+        arrows: false,
+        prevArrow: '<button type="button" class="reviews__nav--prev"><svg><use xlink:href="#chevron"></use></svg></button>',
+        nextArrow: '<button type="button" class="reviews__nav--next"><svg><use xlink:href="#chevron"></use></svg></button>',
+        appendArrows: '.reviews__nav',
+        responsive: [
+            {
+                breakpoint: utils.SCREEN_SIZES.smallDesktop,
+                settings: {
+                    centerPadding: "20%",
+                    arrows: true,
+                    centerMode: true,
                 }
-            ]
-        });
-        $reviewsContainer.on('breakpoint', onResize_);
-    // } else if($reviewsContainer.hasClass('slick-initialized') && $(window).width() < utils.SCREEN_SIZES.mobile) {
-    //     $reviewsContainer.slick('unslick');
-    // }
+            },
+            {
+                breakpoint: utils.SCREEN_SIZES.tablet,
+                settings: {
+                    centerMode: true,
+                    centerPadding: "10%",
+                    arrows: true,
+                }
+            }
+        ]
+    });
+    $reviewsContainer.on('breakpoint', onResize_);
+    $reviewsContainer.on('init', onResize_);
 }
 
 function onResize_() {
     let $slides = $reviewsContainer.find('.slick-slide');
     $slides.height($slides.height());
-    // initSlick_();
 }
